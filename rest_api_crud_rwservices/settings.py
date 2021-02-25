fabad156c0693fa23e491f176709217e26371d89
Hector-Home <hecgarciann@gmail.com>
2021-02-24 21:13:39 -0600

    despliegue2

--- rest_api_crud_rwservices/settings.py
+++ rest_api_crud_rwservices/settings.py
@@ -1,3 +1,9 @@
+import  django_heroku
+
+
+
+
+
 """
 Django settings for rest_api_crud_rwservices project.
 
@@ -143,3 +149,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
