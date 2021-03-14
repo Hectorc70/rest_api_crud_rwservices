61f4a513103bbad5f698313737d8771b323d5b2d
hectorG <hecgarciann>
2021-03-13 22:33:02 -0600

    url endpoint pictures

--- company/urls.py
+++ company/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('api-area/<company>', AreaRecordView.as_view(), name='areas'),
     path('api-activities-area/<area>', ActivitysAreaRecordView.as_view(), name='activities_area'),
     path('api-activity/<id_activity>', ActivityRecordView.as_view(), name='activity'),
-    path('api-picture-activity/<id_activity>', PictureRecordView.as_view(), name='picture'),
+    path('api-pictures-activity/<id_activity>', PictureRecordView.as_view(), name='picture'),
     path('api-tasks-activity/<id_activity>', TaskRecordView.as_view(), name='task'),
 ] 
