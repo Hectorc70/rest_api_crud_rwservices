d3b73898aae6c11b47ab7e12b22f2992b5ed0263
hectorG <hecgarciann>
2021-03-15 12:45:44 -0600

    configs

--- rest_api_crud_rwservices/settings.py
+++ rest_api_crud_rwservices/settings.py
@@ -18,7 +18,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-#from decouple import config
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -102,16 +102,16 @@ WSGI_APPLICATION = 'rest_api_crud_rwservices.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 
     
 }
-"""
 
-import configparser
+
+""" import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -124,7 +124,7 @@ DATABASES = {
             'HOST': config['TEST']['DB_HOST'],
             'PORT': config['TEST']['DB_PORT'],
         }
-}
+} """
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
