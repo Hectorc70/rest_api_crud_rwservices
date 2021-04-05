9f6bc570b2675008eeea24a953fac054073ceb20
hectorG <hecgarciann>
2021-04-05 00:03:43 -0500

    urls para los post de los modelos, y la url de home se redireccioanara a el admin de django

--- rest_api_crud_rwservices/urls.py
+++ rest_api_crud_rwservices/urls.py
@@ -23,7 +23,7 @@ from rest_framework.authtoken import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include('users.urls', namespace='api')),
     path('api/', include('company.urls', namespace='companys')),
         
