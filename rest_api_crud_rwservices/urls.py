0ba40f6106de28fe63a794e55f6822542abe1312
hectorG <hecgarciann>
2021-04-12 20:44:37 -0500

    urls

--- rest_api_crud_rwservices/urls.py
+++ rest_api_crud_rwservices/urls.py
@@ -24,7 +24,7 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
-    path('', admin.site.urls),
+    path('', admin.site.urls),i
     path('api/', include('users.urls', namespace='api')),
     path('api/', include('company.urls', namespace='companys')),
     
